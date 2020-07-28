6709cabba7caf6198f4d3b0f032d101ebd598131
btwlouis <56355239+btwlouis@users.noreply.github.com>
2020-07-28 15:27:08 +0200

    Update server.lua
    https://forum.cfx.re/t/release-standalone-notification-script/1464244/4?u=btwlouis
    https://forum.cfx.re/t/release-standalone-notification-script/1464244/3?u=btwlouis

--- server.lua
+++ server.lua
@@ -1,11 +1,7 @@
-ESX = nil
-
-TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-
 RegisterCommand("announce", function(source, args)
     local argString = table.concat(args, " ")
     if argString ~= nil then
-        TriggerClientEvent('notificationsn', -1, "#eb4034", "ANKÜNDIGUNG", argString)
+        TriggerClientEvent('notifications', -1, "#eb4034", "ANKÜNDIGUNG", argString)
     end
 end, true)
 
