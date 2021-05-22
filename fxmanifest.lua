85bffa00d01a15e71e388cb10c801dffc9d3717b
btwlouis <56355239+btwlouis@users.noreply.github.com>
2021-05-22 22:48:18 +0200

    Update and rename __resource.lua to fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,9 +1,13 @@
-resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+fx_version 'adamant'
+game 'gta5'
+
+version '1.0.1'
+author 'btwlouis'
+description 'Simple notification script'
 
 ui_page 'html/ui.html'
 
 client_script 'client.lua'
-
 server_script 'server.lua'
 
 files {
